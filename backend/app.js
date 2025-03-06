67878094ff7da7a46660f48fb7e335cd563c2ba0
vamshi <vamshinaikramavath@gmail.com>
2025-03-06 21:58:47 +0530

    second

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,17 @@ const registrationRoutes = require('./routes/registrationRoutes');
 dotenv.config();
 const app = express();
 
+// Allow all origins during development
 app.use(cors());
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+// Routes
 app.use('/api/registrations', registrationRoutes);
 
 module.exports = app;
